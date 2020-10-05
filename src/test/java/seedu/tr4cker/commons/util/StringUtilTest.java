215332805c78fdd87c03f5f7138c71d0d38f33d9
ethan-l-m-e <cs53hv36sw@privaterelay.appleid.com>
2020-10-05 21:50:13 +0800

    Update Find Command to accept partial matches in searches

--- src/test/java/seedu/tr4cker/commons/util/StringUtilTest.java
+++ src/test/java/seedu/tr4cker/commons/util/StringUtilTest.java
@@ -93,9 +93,9 @@ public class StringUtilTest {
      *   - last word in sentence
      *   - middle word in sentence
      *   - matches multiple words
+     *   - query word matches part of a sentence word
      *
      * Possible scenarios returning false:
-     *   - query word matches part of a sentence word
      *   - sentence word matches part of the query word
      *
      * The test method below tries to verify all above with a reasonably low number of test cases.
@@ -109,7 +109,7 @@ public class StringUtilTest {
         assertFalse(StringUtil.containsWordIgnoreCase("    ", "123"));
 
         // Matches a partial word only
-        assertFalse(StringUtil.containsWordIgnoreCase("aaa bbb ccc", "bb")); // Sentence word bigger than query word
+        assertTrue(StringUtil.containsWordIgnoreCase("aaa bbb ccc", "bb")); // Sentence word bigger than query word
         assertFalse(StringUtil.containsWordIgnoreCase("aaa bbb ccc", "bbbb")); // Query word bigger than sentence word
 
         // Matches word in the sentence, different upper/lower case letters
