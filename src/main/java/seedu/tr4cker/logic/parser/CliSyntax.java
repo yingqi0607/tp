7e279af30342bdf8cae7e97b7acc84b663fd0ddf
hanweic53 <hanwei_cheak@hotmail.sg>
2020-10-13 09:49:07 +0800

    Update UI of Homepage and add fonts

--- src/main/java/seedu/tr4cker/logic/parser/CliSyntax.java
+++ src/main/java/seedu/tr4cker/logic/parser/CliSyntax.java
@@ -7,7 +7,7 @@ public class CliSyntax {
 
     /* Prefix definitions */
     public static final Prefix PREFIX_NAME = new Prefix("n/");
-    public static final Prefix PREFIX_DEADLINE = new Prefix("dt/");
+    public static final Prefix PREFIX_DEADLINE = new Prefix("dl/");
     public static final Prefix PREFIX_TAG = new Prefix("t/");
     public static final Prefix PREFIX_TASK_DESCRIPTION = new Prefix("des/");
     public static final Prefix PREFIX_COMPLETION_STATUS = new Prefix("p/");
