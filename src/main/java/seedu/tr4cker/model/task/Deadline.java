2b06334957c601a699985486cfa76d16b9d2b6c2
hanweic53 <hanwei_cheak@hotmail.sg>
2020-10-20 12:47:54 +0800

    Update message in Deadline class

--- src/main/java/seedu/tr4cker/model/task/Deadline.java
+++ src/main/java/seedu/tr4cker/model/task/Deadline.java
@@ -15,10 +15,12 @@ public class Deadline {
 
     public static final String MESSAGE_CONSTRAINTS =
             "Invalid format to input Deadline. Please ensure the following:\n"
-                    + "1. Enter a valid date\n"
+                    + "1. Enter a valid date and time (in 24Hr)\n"
                     + "2. Formats accepted: dd-MM-yyyy HHmm and dd-MMM-yyyy HHmm\n"
-                    + "(e.g. 25-01-2021 1800 or 25-Jan-2021 1800)\n"
-                    + "3. If deadline time is not entered, it will be set to 2359 by default";
+                    + "(e.g. 25-01-2021 1800 and 25-Jan-2021 1800)\n"
+                    + "3. First alphabet of Month is in upper-case, (e.g. Feb instead of feb)\n"
+                    + "4. Natural dates accepted: days of week (e.g. Today, Monday, Sunday)\n"
+                    + "If deadline date/time is not entered, it will be set to today/2359 by default";
     public static final String MESSAGE_FUTURE_CONSTRAINT =
             "Deadline should be a time in the future";
     public static final String VALIDATION_REGEX_MM = "\\d{2}-\\d{2}-\\d{4} \\d{4}";
