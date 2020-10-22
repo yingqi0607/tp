51c63a89d6fe7e808280a86dcc70716557a172d5
ethan-l-m-e <cs53hv36sw@privaterelay.appleid.com>
2020-10-22 14:29:07 +0800

    Separate ModuleCode from Module

--- src/main/java/seedu/tr4cker/model/module/Module.java
+++ src/main/java/seedu/tr4cker/model/module/Module.java
@@ -5,22 +5,16 @@ import static seedu.tr4cker.commons.util.CollectionUtil.requireAllNonNull;
 
 /**
  * Represents a Module in TR4CKER.
- * Guarantees:
- * immutable;
- * name is valid as declared in {@link #isValidModuleName(String)};
- * code is valid as declared in {@link #isValidModuleCode(String)}
+ * Guarantees: immutable; name is valid as declared in {@link #isValidModuleName(String)}
  */
 public class Module {
 
-    public static final String MODULE_NAME_MESSAGE_CONSTRAINTS =
+    public static final String MESSAGE_CONSTRAINTS =
             "Module names should only contain alphanumeric characters and spaces, and it should not be blank";
-    public static final String MODULE_CODE_MESSAGE_CONSTRAINTS =
-            "Module code should be alphanumeric";
-    public static final String MODULE_NAME_VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
-    public static final String MODULE_CODE_VALIDATION_REGEX = "\\p{Alnum}+";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String moduleName;
-    public final String moduleCode;
+    public final ModuleCode moduleCode;
 
     /**
      * Constructs a {@code Module}.
@@ -28,10 +22,9 @@ public class Module {
      * @param moduleName A valid module name.
      * @param moduleCode A valid module code.
      */
-    public Module(String moduleName, String moduleCode) {
+    public Module(String moduleName, ModuleCode moduleCode) {
         requireAllNonNull(moduleName, moduleCode);
-        checkArgument(isValidModuleName(moduleName), MODULE_NAME_MESSAGE_CONSTRAINTS);
-        checkArgument(isValidModuleCode(moduleCode), MODULE_CODE_MESSAGE_CONSTRAINTS);
+        checkArgument(isValidModuleName(moduleName), MESSAGE_CONSTRAINTS);
         this.moduleName = moduleName;
         this.moduleCode = moduleCode;
     }
@@ -40,14 +33,7 @@ public class Module {
      * Returns true if a given string is a valid module name.
      */
     public static boolean isValidModuleName(String test) {
-        return test.matches(MODULE_NAME_VALIDATION_REGEX);
-    }
-
-    /**
-     * Returns true if a given string is a valid module code.
-     */
-    public static boolean isValidModuleCode(String test) {
-        return test.matches(MODULE_CODE_VALIDATION_REGEX);
+        return test.matches(VALIDATION_REGEX);
     }
 
     /**
@@ -63,12 +49,5 @@ public class Module {
     @Override
     public int hashCode() {
         return moduleCode.hashCode();
-    }
-
-    /**
-     * Format state as text for viewing.
-     */
-    public String toString() {
-        return '{' + moduleCode + '}';
     }
 }
