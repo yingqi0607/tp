13019bb87e3ab39d50add760f51d85ac75800f97
hanweic53 <hanwei_cheak@hotmail.sg>
2020-10-26 21:51:09 +0800

    Update sample data for tasks

--- src/test/java/seedu/tr4cker/model/util/SampleDataUtilTest.java
+++ src/test/java/seedu/tr4cker/model/util/SampleDataUtilTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static seedu.tr4cker.model.util.SampleDataUtil.getModuleCodeSet;
 import static seedu.tr4cker.model.util.SampleDataUtil.getTagSet;
 
-import java.util.HashSet;
-
 import org.junit.jupiter.api.Test;
 
 import seedu.tr4cker.model.module.Module;
@@ -19,30 +17,30 @@ import seedu.tr4cker.model.task.TaskDescription;
 class SampleDataUtilTest {
 
     private final Task[] tasks = new Task[] {
-        new Task(new Name("Expired Task 1"), new Deadline("10-Oct-2019 1800", false),
-               new CompletionStatus(90), new TaskDescription("An expired task"),
-               new HashSet<>(), getTagSet("exp")),
-        new Task(new Name("Expired Task 2"), new Deadline("10-Oct-2019 1800", false),
-               new CompletionStatus(80), new TaskDescription("Another expired task"),
-               new HashSet<>(), getTagSet("exp")),
+        new Task(new Name("GEQ1000 Tutorial 3"), new Deadline("10-Oct-2019 1800", false),
+                new CompletionStatus(90), new TaskDescription("Too much reasoning"),
+                getModuleCodeSet("GEQ1000"), getTagSet("tutorial", "origami")),
+        new Task(new Name("GER1000 Graded Assignment 4"), new Deadline("20-Oct-2019 1800", false),
+                new CompletionStatus(80), new TaskDescription("Chapter 3 - Sampling Methods"),
+                getModuleCodeSet("GER1000"), getTagSet("graded", "assignment")),
         new Task(new Name("CS1101S Mission"), new Deadline("10-Oct-2021 1800", false),
                 new CompletionStatus(100), new TaskDescription("A very fun mission"),
-                getModuleCodeSet("CS1101S"), getTagSet("CS1101S", "Mission", "Fun")),
+                getModuleCodeSet("CS1101S"), getTagSet("Mission", "Fun")),
         new Task(new Name("CS1101S Quiz"), new Deadline("11-Sep-2021 2359", false),
                 new CompletionStatus(0), new TaskDescription("A quiz that's too hard for me"),
-                getModuleCodeSet("CS1101S"), getTagSet("CS1101S", "Quiz", "2Hard4Me")),
+                getModuleCodeSet("CS1101S"), getTagSet("Quiz", "2Hard4Me")),
         new Task(new Name("CS2100 MidTerms"), new Deadline("12-Oct-2021 1200", false),
                 new CompletionStatus(20), new TaskDescription("Need do cheat sheet"),
                 getModuleCodeSet("CS2100"), getTagSet("cheatSheetsFTW")),
         new Task(new Name("CS2103T Project"), new Deadline("13-Sep-2021 1500", false),
                 new CompletionStatus(50), new TaskDescription("I love project work"),
-                getModuleCodeSet("CS2103T"), getTagSet("CS2103T", "EthanSGKK")),
+                getModuleCodeSet("CS2103T"), getTagSet("EthanSGKK")),
         new Task(new Name("CS2101 OP2"), new Deadline("14-Apr-2021 1010", false),
                 new CompletionStatus(0), new TaskDescription("All the best Good Luck"),
-                getModuleCodeSet("CS2101"), getTagSet("CS2101", "20Percent")),
+                getModuleCodeSet("CS2101"), getTagSet("20Percent")),
         new Task(new Name("CS1231S Graded Assignment"), new Deadline("25-Dec-2021 0000", false),
                 new CompletionStatus(1), new TaskDescription("JustDueet"),
-                getModuleCodeSet("CS1231S"), getTagSet("CS1231S", "Assignment"))
+                getModuleCodeSet("CS1231S"), getTagSet("Assignment"))
     };
 
     private final Module[] modules = new Module[] {
