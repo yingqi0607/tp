9f0b9467c4936450083737440fc68d26e7c616c5
hanweic53 <hanwei_cheak@hotmail.sg>
2020-11-05 20:55:05 +0800

    Add tests for DeleteCommandParser

--- src/main/java/seedu/tr4cker/logic/parser/ParserUtil.java
+++ src/main/java/seedu/tr4cker/logic/parser/ParserUtil.java
@@ -30,7 +30,7 @@ public class ParserUtil {
 
     public static final String MESSAGE_INVALID_INDEX = "Index is not a non-zero unsigned integer.";
 
-    public static final String MESSAGE_INVALID_DAYS = "Number of days given is not a non-zero  unsigned integer.";
+    public static final String MESSAGE_INVALID_DAYS = "Number of days given is not a non-zero unsigned integer.";
     /**
      * Parses {@code oneBasedIndex} into an {@code Index} and returns it. Leading and trailing whitespaces will be
      * trimmed.
